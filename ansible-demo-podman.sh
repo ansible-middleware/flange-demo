b306647a0f4b650af428cf66074423534a46b03e
Guido Grazioli <ggraziol@redhat.com>
2021-11-24 08:03:34 -0500

    EAP RHSSO support
    Also includes changes for postgresql in containers and reverts
    a change in wildfly that was breaking app deployments

--- ansible-demo-podman.sh
+++ ansible-demo-podman.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+# create dnsname enabled podman network
+# podman network create 3trains
+# { "capabilities": { "aliases": true }, "domainName": "dns.podman", "type": "dnsname" }
 
-podman run --name=jbcs-0     --systemd=true  --workdir /work -v $(pwd):/work:rw  -dit localhost/ubi8/ubi-ansible-3trains-demo:latest /sbin/init
-podman run --name=jdg-0      --systemd=true  --workdir /work -v $(pwd):/work:rw  -dit localhost/ubi8/ubi-ansible-3trains-demo:latest /sbin/init
-podman run --name=keycloak-0 --systemd=true  --workdir /work -v $(pwd):/work:rw  -dit localhost/ubi8/ubi-ansible-3trains-demo:latest /sbin/init
-podman run --name=pgsql-0    --systemd=true  --workdir /work -v $(pwd):/work:rw  -dit localhost/ubi8/ubi-ansible-3trains-demo:latest /sbin/init
-podman run --name=wfly-1     --systemd=true  --workdir /work -v $(pwd):/work:rw  -dit localhost/ubi8/ubi-ansible-3trains-demo:latest /sbin/init
-podman run --name=wfly-2     --systemd=true  --workdir /work -v $(pwd):/work:rw  -dit localhost/ubi8/ubi-ansible-3trains-demo:latest /sbin/init
-podman run --name=wfly-3     --systemd=true  --workdir /work -v $(pwd):/work:rw  -dit localhost/ubi8/ubi-ansible-3trains-demo:latest /sbin/init
+podman run --name=jbcs-0     --network 3trains --systemd=true  --workdir /work -v $(pwd):/work:rw  -dit localhost/ubi8/ubi-ansible-3trains-demo:latest /sbin/init
+podman run --name=jdg-0      --network 3trains --systemd=true  --workdir /work -v $(pwd):/work:rw  -dit localhost/ubi8/ubi-ansible-3trains-demo:latest /sbin/init
+podman run --name=keycloak-0 --network 3trains --systemd=true  --workdir /work -v $(pwd):/work:rw  -dit localhost/ubi8/ubi-ansible-3trains-demo:latest /sbin/init
+podman run --name=pgsql-0    --network 3trains --systemd=true  --workdir /work -v $(pwd):/work:rw  -dit localhost/ubi8/ubi-ansible-3trains-demo:latest /sbin/init
+podman run --name=wfly-1     --network 3trains --systemd=true  --workdir /work -v $(pwd):/work:rw  -dit localhost/ubi8/ubi-ansible-3trains-demo:latest /sbin/init
+podman run --name=wfly-2     --network 3trains --systemd=true  --workdir /work -v $(pwd):/work:rw  -dit localhost/ubi8/ubi-ansible-3trains-demo:latest /sbin/init
+podman run --name=wfly-3     --network 3trains --systemd=true  --workdir /work -v $(pwd):/work:rw  -dit localhost/ubi8/ubi-ansible-3trains-demo:latest /sbin/init
 ANSIBLE_CONFIG=podman/ansible-podman.cfg ansible-playbook -e @rhn-creds.yml -i inventory/demo playbooks/demo.yml "$@"
 
 
