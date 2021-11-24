5d1205bbb5ec6f60e31460ec7520e0e4a5b40265
Guido Grazioli <ggraziol@redhat.com>
2021-11-24 11:14:12 -0500

    Use args in podman dockerfile

--- podrun.sh
+++ podrun.sh
@@ -3,7 +3,7 @@
 readonly DOCKER_IMAGE=${DOCKER_IMAGE:-'ubi8-ansible'}
 readonly DOCKER_NAME=${1:-'demo'}
 
-if [ ! "$(docker ps -q -f name=${DOCKER_NAME})" ]; then
+if [ ! "$(podman ps -q -f name=${DOCKER_NAME})" ]; then
   podman run  -dit --systemd=true --privileged=true  \
        --rm --name "${DOCKER_NAME}" --workdir /work -v $(pwd):/work:rw \
        "${DOCKER_IMAGE}" \
