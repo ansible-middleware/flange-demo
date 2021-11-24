9288fa493a4f49af93a0c4c76a377c63fcafe008
Guido Grazioli <ggraziol@redhat.com>
2021-11-24 08:21:23 -0500

    Move some files around and update README

--- ansible-demo-podman.sh
+++ ansible-demo-podman.sh
@@ -8,7 +8,6 @@ podman run --name=pgsql-0    --systemd=true  --workdir /work -v $(pwd):/work:rw 
 podman run --name=wfly-1     --systemd=true  --workdir /work -v $(pwd):/work:rw  -dit localhost/ubi8/ubi-ansible-3trains-demo:latest /sbin/init
 podman run --name=wfly-2     --systemd=true  --workdir /work -v $(pwd):/work:rw  -dit localhost/ubi8/ubi-ansible-3trains-demo:latest /sbin/init
 podman run --name=wfly-3     --systemd=true  --workdir /work -v $(pwd):/work:rw  -dit localhost/ubi8/ubi-ansible-3trains-demo:latest /sbin/init
-#ANSIBLE_CONFIG=ansible.cfg-podman ansible-playbook -e rhn_username="'"${RHN_USERNAME}"'" -e rhn_password="'"${RHN_PASSWORD}"'" -i inventory/demo playbooks/demo.yml  -vv
-ANSIBLE_CONFIG=ansible-podman.cfg ansible-playbook -e @rhn-creds.yml -i inventory/demo playbooks/demo.yml "$@"
+ANSIBLE_CONFIG=podman/ansible-podman.cfg ansible-playbook -e @rhn-creds.yml -i inventory/demo playbooks/demo.yml "$@"
 
 
