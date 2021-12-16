ae76e93ca475c3669326aec010f7de032104f617
Guido Grazioli <ggraziol@redhat.com>
2021-12-16 13:59:48 +0100

    podman: fix jq filter for inventory hosts

--- ansible-demo-podman.sh
+++ ansible-demo-podman.sh
@@ -7,6 +7,6 @@
 while read node ; do
     echo -n "Starting container ${node}.... "
     podman run --name=${node} --network 3trains --systemd=true  --workdir /work -v $(pwd):/work:rw  -d localhost/ubi8/ubi-ansible-3trains-demo:latest /sbin/init
-done < <( ansible-inventory -i inventory/demo --list | jq -r "._meta.hostvars | keys[]")
+done < <( ansible-inventory -i inventory/demo --list | jq -r ".[].hosts|select(length > 0)|.[]")
 podman network reload --all 2>&1 >/dev/null
 ANSIBLE_CONFIG=podman/ansible-podman.cfg ansible-playbook -e @rhn-creds.yml -i inventory/demo playbooks/demo.yml "$@"
